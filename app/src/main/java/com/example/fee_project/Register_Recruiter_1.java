fb4e0b482aab240c63a2672280329181ec43d1d6
Youness_JABAR <74419316+Youness-JABAR@users.noreply.github.com>
2020-12-25 22:07:09 +0100

    recruiter 2

--- app/src/main/java/com/example/fee_project/Register_Recruiter_1.java
+++ app/src/main/java/com/example/fee_project/Register_Recruiter_1.java
@@ -1,5 +1,6 @@
 package com.example.fee_project;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,6 +16,13 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.net.URI;
 
@@ -23,6 +31,7 @@ public class Register_Recruiter_1 extends AppCompatActivity {
     EditText insre_nom,insre_prenom,insre_email,insre_password,insre_password2,insre_entreprise,insre_description;
     ImageView iv_ent_photo,iv_rct_logo;
     Button btn_ent_photo,btn_insre;
+    StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,6 +50,8 @@ public class Register_Recruiter_1 extends AppCompatActivity {
         btn_ent_photo = findViewById(R.id.btn_ent_photo);
         iv_ent_photo = findViewById(R.id.iv_ent_photo);
 
+        storageReference= FirebaseStorage.getInstance().getReference();
+
         btn_ent_photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -57,7 +68,23 @@ public class Register_Recruiter_1 extends AppCompatActivity {
             if (resultCode == Activity.RESULT_OK){
                 Uri ImageUri = data.getData();
                 iv_ent_photo.setImageURI(ImageUri);
+                uploadImageToFirebase(ImageUri);
             }
         }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri ) {
+        StorageReference fileRef=storageReference.child("profile.jpg");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Toast.makeText(Register_Recruiter_1.this, "img uploaded", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Register_Recruiter_1.this, "failed", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
