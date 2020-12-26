415701b15a9afe868acf6ee0a7b06a7d20febb50
Youness_JABAR <74419316+Youness-JABAR@users.noreply.github.com>
2020-12-26 22:59:02 +0100

    add_plus_offre

--- app/src/main/java/com/example/fee_project/Student_accueil.java
+++ app/src/main/java/com/example/fee_project/Student_accueil.java
@@ -2,6 +2,7 @@ package com.example.fee_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -26,10 +27,12 @@ public class Student_accueil extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.item1:
-                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Item 1 ", Toast.LENGTH_SHORT).show();
                 return true;
             case R.id.item2:
-                Toast.makeText(this, "Item 2 selected", Toast.LENGTH_SHORT).show();
+                Intent i=new Intent(Student_accueil.this,Login.class);
+                startActivity(i);
+                Toast.makeText(this, "deconnexion", Toast.LENGTH_SHORT).show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
