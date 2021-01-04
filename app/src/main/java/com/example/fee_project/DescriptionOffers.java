46d80e195f24dc7ba8db2ce45cc01e22786fb4ba
Youness_JABAR <74419316+Youness-JABAR@users.noreply.github.com>
2021-01-04 16:03:35 +0100

    Description offer

---
+++ app/src/main/java/com/example/fee_project/DescriptionOffers.java
@@ -0,0 +1,14 @@
+package com.example.fee_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DescriptionOffers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_description_offers);
+    }
+}
