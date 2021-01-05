f11141338f81d39daacd6b82746d5f5fca559e84
oumaimasafi <76428646+oumaimasafi@users.noreply.github.com>
2021-01-05 20:50:44 +0100

    fct_update

--- app/src/main/java/com/example/fee_project/OffreAdapter.java
+++ app/src/main/java/com/example/fee_project/OffreAdapter.java
@@ -1,23 +1,35 @@
 package com.example.fee_project;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.BreakIterator;
 import java.util.ArrayList;
 
 public class OffreAdapter extends RecyclerView.Adapter<OffreAdapter.MyViewHolder>{
+
     private Context context;
-    private ArrayList titre,description;
-    OffreAdapter(Context context, ArrayList titre, ArrayList description){
+    Activity activity;
+    private ArrayList idoff,titre,type,remuneration,description,periode;
+
+    public OffreAdapter(Context context, Activity activity, ArrayList idoff, ArrayList titre, ArrayList type, ArrayList remuneration, ArrayList description, ArrayList periode) {
         this.context = context;
+        this.activity = activity;
+        this.idoff = idoff;
         this.titre = titre;
+        this.type = type;
+        this.remuneration = remuneration;
         this.description = description;
+        this.periode = periode;
     }
 
 
@@ -31,8 +43,27 @@ public class OffreAdapter extends RecyclerView.Adapter<OffreAdapter.MyViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        holder.idoff.setText(String.valueOf(idoff.get(position)));
         holder.titre.setText(String.valueOf(titre.get(position)));
+        holder.type.setText(String.valueOf(type.get(position)));
+        holder.remuneration.setText(String.valueOf(remuneration.get(position)));
         holder.description.setText(String.valueOf(description.get(position)));
+        holder.periode.setText(String.valueOf(periode.get(position)));
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(context, UpdateOffre.class);
+                intent.putExtra("idoff", String.valueOf(idoff.get(position)));
+                intent.putExtra("title", String.valueOf(titre.get(position)));
+                intent.putExtra("type", String.valueOf(type.get(position)));
+                intent.putExtra("remuneration", String.valueOf(remuneration.get(position)));
+                intent.putExtra("description", String.valueOf(description.get(position)));
+                intent.putExtra("periode", String.valueOf(periode.get(position)));
+                activity.startActivityForResult(intent,1);
+                //when we click in our reclycle view item, on transfere ses donnees a updateoffre
+            }
+        });
     }
 
     @Override
@@ -41,12 +72,19 @@ public class OffreAdapter extends RecyclerView.Adapter<OffreAdapter.MyViewHolder
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        TextView titre,description;
+
+        TextView idoff,titre,type,remuneration,description,periode;
+        LinearLayout mainLayout;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            idoff = itemView.findViewById(R.id.tv_id);
             titre = itemView.findViewById(R.id.tv_titre);
+            type = itemView.findViewById(R.id.tv_typeoff);
+            remuneration = itemView.findViewById(R.id.tv_remunerationoff);
             description = itemView.findViewById(R.id.tv_description);
+            periode = itemView.findViewById(R.id.tv_periodeoff);
+            mainLayout = itemView.findViewById(R.id.mainLayout);
         }
 
     }
---
+++ app/src/main/java/com/example/fee_project/UpdateOffre.java
@@ -0,0 +1,141 @@
+package com.example.fee_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class UpdateOffre extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    EditText titre,description,periode;
+    Spinner type;
+    RadioButton radioButton;
+
+    //Spinner type;
+    Button btn_update;
+    RadioButton oui,non;
+   RadioGroup rdgrp2;
+    String title_up, description_up,id_up,periode_up,type_up,remuneration_up;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_offre);
+
+        titre=findViewById(R.id.offre_titre2);
+        type=findViewById(R.id.spinner2);
+
+
+        ArrayAdapter<CharSequence> adapter;
+        adapter = ArrayAdapter.createFromResource(this,R.array.types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        type.setAdapter(adapter);
+        type.setOnItemSelectedListener(this);
+
+
+        periode=findViewById(R.id.offre_periode2);
+        description=findViewById(R.id.offre_description2);
+        oui=findViewById(R.id.offre_btnoui2);
+        non=findViewById(R.id.offre_btnnon2);
+        rdgrp2=findViewById(R.id.roleRadioGroup2);
+        btn_update=findViewById(R.id.offre_update);
+        //appel de la méthode
+        getAndSetIntentDataRec();
+        btn_update.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+               String nvtitre=titre.getText().toString();// cas titre qui est dans la page xml de updateoffre , sa valeur qui est  vide va etre modifié par la valeur qu'ajouté le recruteur q"on a récuépér par intent , et put extra
+                String nvdescription=description.getText().toString();
+                String nvperiode=periode.getText().toString();
+
+                DataBaseHelper db =new DataBaseHelper(UpdateOffre.this);
+                //appel de la fct de modification
+
+                db.updateData(id_up, nvtitre,nvtype,nvrem,nvdescription,nvperiode);
+
+            }
+        });
+
+
+    }
+    ////on recupere les donnees qu'on a fait passer par intent - putextra)
+    void getAndSetIntentDataRec(){
+        if (getIntent().hasExtra("idoff")){
+            ////on recupere les donnees qu'on a fait passer par intent - putextra)
+///// and store the information inside the strings
+            id_up=getIntent().getStringExtra("idoff");
+            Toast.makeText(this,id_up,Toast.LENGTH_SHORT).show();
+            title_up=getIntent().getStringExtra("title");
+           type_up=getIntent().getStringExtra("type");
+           remuneration_up=getIntent().getStringExtra("remuneration");
+
+            Toast.makeText(this,remuneration_up,Toast.LENGTH_SHORT).show();
+            description_up=getIntent().getStringExtra("description");
+
+            periode_up=getIntent().getStringExtra("periode");
+
+            int valeurtype=getnumtype(type_up);
+            //SETTING DATA
+            titre.setText(title_up);// cas titre qui est dans la page xml de updateoffre , sa valeur qui est  vide va etre modifié par la valeur qu'ajouté le recruteur q"on a récuépér par intent , et put extra
+            description.setText(description_up);
+            type.setSelection(valeurtype);
+
+            if(remuneration_up.compareTo("oui")==0){
+                rdgrp2.check(R.id.offre_btnoui2);
+            }
+            else {
+                rdgrp2.check(R.id.offre_btnnon2);
+            }
+
+
+
+            periode.setText(periode_up);
+
+        }
+        else{  Toast.makeText(this,"No data",Toast.LENGTH_SHORT).show();}
+    }
+
+    private int getnumtype(String type_up) {
+        if(type_up=="OBSERVATION")
+            return 0;
+        else if(type_up=="PFA")
+        return 1;
+        else if(type_up=="PFE")
+            type.setSelection(2);
+        return 2;
+    }
+
+    String nvtype;
+    @Override
+
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        nvtype = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), nvtype , Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+
+    String nvrem;
+    public void checkButton(View v) {
+        int radioId = rdgrp2.getCheckedRadioButtonId();
+        radioButton =findViewById(radioId);
+        nvrem=radioButton.getText().toString();
+        Toast.makeText(this,"le bouton selectionne est :"+radioButton.getText(),Toast.LENGTH_SHORT).show();
+
+
+    }
+}
+
